a3d683b9c10958ff0891799f4efabe87ced7b4c7
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-11 00:04:41 +0700

    Refactor countdown logic and add dynamic name from URL

--- components/Section1.vue
+++ components/Section1.vue
@@ -1,48 +1,52 @@
+<!-- eslint-disable curly -->
 <!-- eslint-disable ts/no-use-before-define -->
 <script setup>
 const hariH = new Date('2024-01-21')
+const now = ref(new Date())
 
 const countdown = computed(() => {
-  const now = new Date()
-  const diff = hariH - now
+  const diff = hariH - now.value
+  if (diff < 0) {
+    return 'Waktu telah berlalu'
+  }
+
   const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-  const hours = Math.floor((diff - (days * (1000 * 60 * 60 * 24))) / (1000 * 60 * 60))
-  const minutes = Math.floor((diff - (days * (1000 * 60 * 60 * 24)) - (hours * (1000 * 60 * 60))) / (1000 * 60))
-  const seconds = Math.floor((diff - (days * (1000 * 60 * 60 * 24)) - (hours * (1000 * 60 * 60)) - (minutes * (1000 * 60))) / 1000)
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000)
 
   return `${days} hari, ${hours} jam --- ${minutes} menit, ${seconds} detik`
 })
 
+setInterval(() => {
+  now.value = new Date()
+}, 1000)
+
+
+
 const nama = computed(() => {
   const url = window.location.href
-
-  // Cek apakah string url mengandung karakter #
   const index = url.indexOf('#')
 
-  // Jika tidak, kembalikan nilai default
-  const namaLengkap = index === -1 ? defaultNama : url.split('#')[0]
+  if (index !== -1) {
+    const namaLengkap = url.substring(index + 1)
+    return decodeURIComponent(namaLengkap)
+  }
 
-  return decodeURIComponent(namaLengkap.split(' ')[0])
+  return null // Kembali null jika tidak ada nama di URL
 })
-
-const defaultNama = 'Teman-teman'
 </script>
 
 <template>
   <div class="px-4 py-24 text-center md:px-10">
     <div>
-      <div class="p-4 rounded-md bg-2 text-3">
+      <div v-if="nama" class="p-4 rounded-md bg-2 text-3">
+        <!-- Hanya tampilkan jika ada nama -->
         <h2>Undangan Kepada</h2>
-        <p v-if="nama">
-          {{ nama }}
-        </p>
-        <p v-else>
-          Undangan untuk {{ defaultNama }}
-        </p>
+        <p>{{ nama }}</p>
       </div>
     </div>
     <h2>Waktu Menuju Hari H</h2>
     <p>{{ countdown }}</p>
-
   </div>
 </template>
